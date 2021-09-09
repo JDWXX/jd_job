9e45b3f590f2108463d85730f7508078433a2192
246771270 <246771270@qq.com>
2021-09-09 15:59:36 +0800

    2.9

--- py/jd_wskey.py
+++ py/jd_wskey.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 '''
-项目名称: JD-Script / jd_qjd
-Author: Curtin
-功能：全民抢京豆（7.22-7.31）：https://h5.m.jd.com/rn/3MQXMdRUTeat9xqBSZDSCCAE9Eqz/index.html?has_native=0
-    满160豆需要20人助力，每个用户目前只能助力2次不同的用户。
-Date: 2021/7/3 上午10:02
-TG交流 https://t.me/topstyle996
-TG频道 https://t.me/TopStyle2021
-update: 2021.7.22 16:55
-* 修复了助力活动不存在、增加了随机UA（如果未定义ua则启用随机UA）
-* 新增推送
-* 修复0点不能开团
-* 兼容pin为中文转码编码
+cron: 15 */16 * * * jd_wskey.py
+new Env('wskey转换');
 '''
 
 import zlib, base64
