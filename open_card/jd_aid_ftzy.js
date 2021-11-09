9273b126eb1b5a41403d661387115d4d9f32ff1e
246771270 <246771270@qq.com>
2021-11-09 09:38:01 +0800

    2021.11.18

--- open_card/jd_aid_ftzy.js
+++ open_card/jd_aid_ftzy.js
@@ -1,6 +1,6 @@
 /**
-cron=15 3,9 * * * jd_aid_ftzy.js
-new Env('京东沸腾之夜助力');
+ cron=15 3,9 * * * jd_aid_ftzy.js
+ new Env('京东沸腾之夜助力');
  */
 let common = require("./function/common");
 let $ = new common.env('京东沸腾之夜助力');
