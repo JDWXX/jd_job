9c632b1c223d639acd249d7a7c9b4e62722f95fd
246771270 <246771270@qq.com>
2021-11-12 13:43:55 +0800

    赚京豆

--- jd/jd_syj.js
+++ jd/jd_syj.js
@@ -46,13 +46,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  $.authorTuanList = await getAuthorShareCode('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/jd_zz.json');
-  if (!$.authorTuanList) {
-    $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jd_zz.json'}).then((resp) => {}).catch((e) => $.log('刷新CDN异常', e));
-    await $.wait(1000)
-    $.authorTuanList = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jd_zz.json') || [];
-  }
-  // await getRandomCode();
+  $.authorTuanList = [];
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -727,26 +721,6 @@ function getAuthorShareCode(url) {
       }
     })
   })
-}
-async function getRandomCode() {
-  await $.http.get({url: `http://go.chiang.fun/read/zuan/${randomCount}`, timeout: 10000}).then(async (resp) => {
-    if (resp.statusCode === 200) {
-      try {
-        let { body } = resp;
-        body = JSON.parse(body);
-        if (body && body['code'] === 200) {
-          console.log(`随机取【赚京豆-瓜分京豆】${randomCount}个邀请码成功\n`);
-          $.body = body['data'];
-          $.body1 = [];
-          $.body.map(item => {
-            $.body1.push(JSON.parse(item));
-          })
-        }
-      } catch (e) {
-        console.log(`随机取【赚京豆-瓜分京豆】${randomCount}个邀请码异常:${e}`);
-      }
-    }
-  }).catch((e) => console.log(`随机取【赚京豆-瓜分京豆】${randomCount}个邀请码异常:${e}`));
 }
 //======================赚京豆开团===========END=====
 function taskUrl(function_id, body = {}) {
