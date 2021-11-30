9f5bbd3fff93e3dc9768a255b398d42017481773
246771270 <246771270@qq.com>
2021-11-30 14:10:59 +0800

    py 推送

--- py/jd_qjd.py
+++ py/jd_qjd.py
@@ -92,7 +92,7 @@ class msg(object):
         if a == 0:
             a += 1
         try:
-            url = 'https://gitee.com/curtinlv/Public/raw/master/sendNotify.py'
+            url = 'https://ghproxy.com/https://github.com/JDWXX/jd_job/blob/master/sendNotify.py'
             response = requests.get(url)
             if 'curtinlv' in response.text:
                 with open('sendNotify.py', "w+", encoding="utf-8") as f:
