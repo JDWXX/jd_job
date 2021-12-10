0555d8ba9565b221d7770442b93f7bd9373070d4
246771270 <246771270@qq.com>
2021-12-10 18:10:32 +0800

    京喜财富岛合成月饼-助力

--- jd/jd_cfd_mooncake_zl.js
+++ jd/jd_cfd_mooncake_zl.js
@@ -109,7 +109,7 @@ async function cfd() {
   try {
     nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000)
     let beginInfo = await getUserInfo();
-    if (beginInfo.LeadInfo.dwLeadType === 2) {
+    if (beginInfo.LeadInfo && beginInfo.LeadInfo.dwLeadType === 2) {
       console.log(`还未开通活动，尝试初始化`)
       await noviceTask()
       await $.wait(2000)
