01d538581281a874144380d78010a31d23de91eb
246771270 <246771270@qq.com>
2021-12-13 14:26:58 +0800

    京喜购物返红包助力

--- open_card/jd_aid_cashback.js
+++ open_card/jd_aid_cashback.js
@@ -1,4 +1,4 @@
-let common = require("../function/common");
+let common = require("./function/common");
 const $ = new Env('京喜购物返红包助力');
 let min = 5,
     help = $.config[$.filename(__filename)] || Math.min(min, $.config.JdMain) || min;
