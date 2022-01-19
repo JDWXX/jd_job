64ee76d87e0ff829da3b3cb19e04346a5bb55c47
246771270 <246771270@qq.com>
2022-01-19 18:03:52 +0800

    wskey转换 10114版本支持二个京东接口转换

--- py/jd_wskey.py
+++ py/jd_wskey.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*
 '''
-cron: 15 2 * * * jd_wskey.py
+cron: 0 0 0-8 * * * jd_wskey.py
 new Env('wskey转换');
 '''
 
@@ -28,7 +28,7 @@ try:
 except:
     logger.info("无推送文件")
 
-ver = 10102
+ver = 10114
 
 
 # 登录青龙 返回值 token
@@ -275,21 +275,21 @@ def boom():
         logger.info("Verification passed")
         logger.info("--------------------\n")
 
-#
-# def update():
-#     up_ver = int(cloud_arg['update'])
-#     if ver >= up_ver:
-#         logger.info("当前脚本版本: " + str(ver))
-#         logger.info("--------------------\n")
-#     else:
-#         logger.info("当前脚本版本: " + str(ver) + "新版本: " + str(up_ver))
-#         logger.info("存在新版本, 请更新脚本后执行")
-#         logger.info("--------------------\n")
-#         text = '当前脚本版本: {0}新版本: {1}, 请更新脚本~!'.format(ver, up_ver)
-#         try:
-#             send('WSKEY转换', text)
-#         except:
-#             logger.info("通知发送失败")
+
+def update():
+    up_ver = int(cloud_arg['update'])
+    if ver >= up_ver:
+        logger.info("当前脚本版本: " + str(ver))
+        logger.info("--------------------\n")
+    else:
+        logger.info("当前脚本版本: " + str(ver) + "新版本: " + str(up_ver))
+        logger.info("存在新版本, 请更新脚本后执行")
+        logger.info("--------------------\n")
+        text = '当前脚本版本: {0}新版本: {1}, 请更新脚本~!'.format(ver, up_ver)
+        try:
+            send('WSKEY转换', text)
+        except:
+            logger.info("通知发送失败")
         # sys.exit(0)
 
 
@@ -307,7 +307,7 @@ def ql_check(port):
 
 
 # 返回值 bool, key, eid
-def serch_ck(pin):
+def serch_ck_old(pin):
     if all('\u4e00' <= char <= '\u9fff' for char in pin):
         pin1 = urllib.parse.quote(pin)
         pin2 = pin1.replace('%', '%5C%25')
@@ -340,12 +340,52 @@ def serch_ck(pin):
         return True, key, eid
 
 
+def serch_ck(pin):
+    for i in range(len(envlist)):
+        if pin in envlist[i]['value']:
+            value = envlist[i]['value']
+            id = envlist[i][ql_id]
+            logger.info(str(pin) + "检索成功\n")
+            return True, value, id
+        else:
+            continue
+    logger.info(str(pin) + "检索失败\n")
+    return False, 1
+
+
+def get_env():
+    url = 'http://127.0.0.1:{0}/api/envs'.format(port)
+    try:
+        res = s.get(url)
+    except:
+        logger.info("\n青龙环境接口错误")
+        sys.exit(1)
+    else:
+        data = json.loads(res.text)['data']
+        return data
+
+
+def get_version():
+    url = 'http://127.0.0.1:{0}/api/system'.format(port)
+    try:
+        res = s.get(url)
+        version = str(json.loads(res.text)['data']['version'])
+    except:
+        return 0
+    else:
+        logger.info("青龙面板版本: " + version)
+        if version > '2.10.13':
+            return 1
+        else:
+            return 0
+
+
 def ql_update(e_id, n_ck):
     url = 'http://127.0.0.1:{0}/api/envs'.format(port)
     data = {
         "name": "JD_COOKIE",
         "value": n_ck,
-        "_id": e_id
+        ql_id: e_id
     }
     data = json.dumps(data)
     res = json.loads(s.put(url=url, data=data).text)
@@ -423,7 +463,7 @@ def check_cloud():
             continue
         else:
             info = ['Default', 'HTTPS', 'CloudFlare']
-            logger.info(str(info[url_list.index(i)]) + " Server Check OK\n")
+            logger.info(str(info[url_list.index(i)]) + " Server Check OK\n--------------------\n")
             return i
     logger.info("\n云端地址全部失效, 请检查网络!")
     try:
@@ -448,12 +488,13 @@ if __name__ == '__main__':
         logger.info("\n如果你很确定端口没错, 还是无法执行, 在GitHub给我发issus\n--------------------\n")
         sys.exit(1)
     else:
-        logger.info(str(port) + "端口检查通过\n")
+        logger.info(str(port) + "端口检查通过")
     # global cloud_arg
     token = ql_login()  # 获取青龙 token
     s = requests.session()
     s.headers.update({"authorization": "Bearer " + str(token)})
     s.headers.update({"Content-Type": "application/json;charset=UTF-8"})
+    ql_id = ['_id', 'id'][get_version()]
     url_t = check_cloud()
     cloud_arg = cloud_info()
     # update()
@@ -461,6 +502,7 @@ if __name__ == '__main__':
     ua = cloud_arg['User-Agent']
     sv, st, uuid, sign = get_sign()
     wslist = get_wskey()
+    envlist = get_env()
     for ws in wslist:
         wspin = ws.split(";")[0]
         if "pin" in wspin:
