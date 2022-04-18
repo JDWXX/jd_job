21930ad7ebbd2c38b6095c097881a3108b4f3b54
246771270 <246771270@qq.com>
2022-04-18 23:59:17 +0800

    119

--- open_card/jd_opencardL118.js
+++ open_card/jd_opencardL118.js
@@ -206,10 +206,10 @@ async function run() {
       return
     }
     console.log($.actorUuid)
-    console.log(`当前助力:${$.shareUuid}`)
+    console.log(`当前助力:${$.shareUuids}`)
     if($.index == 1){
-      $.shareUuid = $.actorUuid
-      console.log(`后面的号都会助力:${$.shareUuid}`)
+      $.shareUuids = $.actorUuid
+      console.log(`后面的号都会助力:${$.shareUuids}`)
     }
     await $.wait(parseInt(Math.random() * 1000 + 5000, 10))
       if($.index % 3 == 0) console.log('休息一下，别被黑ip了\n可持续发展')
