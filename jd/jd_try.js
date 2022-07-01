f62e87b34644e306ff3c1edac379805836e7e1c4
246771270 <246771270@qq.com>
2022-07-01 19:18:15 +0800

    京东试用

--- jd/jd_try.js
+++ jd/jd_try.js
@@ -136,7 +136,7 @@ let args_xh = {
      * 例如是18件，将会进行第三次获取，直到过滤完毕后为20件才会停止，不建议设置太大
      * 可设置环境变量：JD_TRY_MAXLENGTH
      * */
-	maxLength: process.env.JD_TRY_MAXLENGTH * 1 || 10,
+	maxLength: process.env.JD_TRY_MAXLENGTH * 1 || 50,
 	/*
      * 过滤种草官类试用，某些试用商品是专属官专属，考虑到部分账号不是种草官账号
      * 例如A商品是种草官专属试用商品，下面设置为true，而你又不是种草官账号，那A商品将不会被添加到待提交试用组
